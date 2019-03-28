fbbec38ee5cc3748f3ce294e245d4ed00c3db33e
Nicolas Far <nfar@hexacta.com>
2019-03-28 08:34:58 -0300

    Expose rustcsv configs

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,15 @@ fn main() {
           .about("Combine sort | tail -n in a single command")
           .arg(Arg::with_name("delimiter")
                .short("t")
-               .help("Delimiter to break fields by")
+               .help("Delimiter to break fields by. (Defaults to ','")
                .takes_value(true))
+          .arg(Arg::with_name("quote")
+               .short("q")
+               .help("Quote character. (Defaults to '\"'")
+               .takes_value(true))
+          .arg(Arg::with_name("ignore_header")
+               .short("i")
+               .help("Ignore first line"))
           .arg(Arg::with_name("field")
                .short("k")
                .help("Number of field to sort by")
@@ -23,12 +30,18 @@ fn main() {
                .short("n")
                .help("Number of results to return")
                .takes_value(true))
-          .arg(Arg::with_name("parallel")
-               .help("Number of threads to use")
-               .takes_value(true))
           .get_matches();
 
      let delimiter = matches.value_of("delimiter").unwrap_or(",");
+
+     let delim_char = delimiter.chars().nth(0).unwrap() as u8;
+
+     let quote = matches
+          .value_of("quote")
+          .unwrap_or("\"")
+          .chars()
+          .nth(0)
+          .unwrap() as u8;
      let field_number = matches
           .value_of("field")
           .unwrap_or("0")
@@ -48,20 +61,20 @@ fn main() {
      };
 
      let mut rdr = csv::ReaderBuilder::new()
-          .has_headers(false)
-          .delimiter(delimiter.chars().nth(0).unwrap() as u8)
+          .has_headers(matches.is_present("ignore_header"))
+          .delimiter(delim_char)
+          .quote(quote)
           .from_reader(io::stdin());
      let mut top_sort = TopSort::new(ordering, keep_results);
 
      for result in rdr.records() {
-          if let Ok(record) = result{
+          if let Ok(record) = result {
                let entry = TopSortEntry::new(record.get(field_number).unwrap(), &record).unwrap();
                top_sort.add(entry);
           }
      }
      for value in top_sort.get_result() {
-          let string_vec : Vec<&str> = value.iter().collect();
+          let string_vec: Vec<&str> = value.iter().collect();
           println!("{}", string_vec.join(delimiter));
      }
-
 }
